3b02f0df1db08a0a9cb91c4d5695177837b2207f
Jean C. Contreras <jean.cntrs@gmail.com>
2021-11-22 08:51:29 -0300

    Was added microservice configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.URI_MONGODB, {
+      // useCreateIndex: true,
+      // useFindAndModify: false
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
